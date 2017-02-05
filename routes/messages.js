99dc3cf324fab1000c3b3736ce35acb57d6378a0
Alex Lebedyev <aresobus@gmail.com>
2017-02-05 14:32:40 -0500

    messages list added

--- routes/messages.js
+++ routes/messages.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 const authHelpers = require('../auth/auth-helpers');
 const passport = require('../auth/local');
+var models = require('../db/models/index');
 
 
 
-router.get('/', (req, res, next) => {
-  res.render('user/messages', {
-    user: req.user.dataValues
+router.get('/',  (req, res, next)=> {
+  models.Messages.findAll({}).then(function(messages) {
+    res.render('user/messages', {
+     content: 'content',
+      messages: messages
+    });
   });
 });
-
 
 
 module.exports = router;
